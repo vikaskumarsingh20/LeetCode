e5aeb414eeadbd21688cb4d1aa8c3b0a414d7aef
Vikas Kumar Singh <92667215+RemoverIsBack@users.noreply.github.com>
2022-02-01 19:02:14 +0530

    Create Rotate Array.cpp

---
+++ Rotate Array.cpp
@@ -0,0 +1,48 @@
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k%=n;
+        
+        if(k>0){
+            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(),nums.begin()+k);
+            reverse(nums.begin()+k,nums.end());
+        }
+    }
+}; 
+
+// 2 approach
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         vector<int> temp = nums;
+        
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             nums[(i + k) % nums.size()] = temp[i];
+//         }
+        
+//     }
+// };
