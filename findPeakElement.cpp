0dfcdc16fb9d6bbc2ecd5c0d6daead889816d13e
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2023-01-11 21:11:58 +0530

    Create findPeakElement.cpp

---
+++ findPeakElement.cpp
@@ -0,0 +1,55 @@
+
+162. Find Peak Element
+Medium
+8.2K
+4.2K
+Companies
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+  
+  //CODE 
+class Solution {
+public:
+	int findPeakElement(vector<int>& nums) {
+		int i=0, j=nums.size()-1;
+		while(i<j){
+			int mid=i+(j-i)/2;
+			if(nums[mid]>=nums[mid+1])
+              j = mid;
+			else
+            i = mid+1;
+		}
+		return i;
+	}
+};
+
+// 2nd method
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int index=0;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>nums[i-1])
+              index=i;
+        }
+        return index;
+    }
+};
