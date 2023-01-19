723a55c24d9742796fb683d73d2b914130721344
VikasKumarSingh <vikaskumarsingh0931@gmail.com>
2023-01-19 21:01:02 +0530

    Create subarrayProdLessThanK.cpp

---
+++ Medium/subarrayProdLessThanK.cpp
@@ -0,0 +1,43 @@
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+ 
+
+Example 1:
+
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Example 2:
+
+Input: nums = [1,2,3], k = 0
+Output: 0
+
+/**
+ * The idea is always keep an max-product-window less than K;
+ * Every time add a new number on the right(j), reduce numbers on the left(i), until the subarray product fit less than k again, (subarray could be empty);
+ * Each step introduces x new subarrays, where x is the size of the current window (j + 1 - i);
+ * example:
+ * for window (5, 2, 6), when 6 is introduced, it add 3 new subarray:
+ *       (6)
+ *    (2, 6)
+ * (5, 2, 6)
+ */
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k == 0) return 0;
+        int cnt = 0;
+        int prod = 1;
+        for (int start = 0,end = 0; end < nums.size(); end++) {
+            prod *= nums[end];
+            while (start <= end && prod >= k) {
+                prod /= nums[start++];
+            }
+            // subarray size  -> end-start+1
+            cnt += end - start + 1;
+        }
+        return cnt;
+    }
+};
